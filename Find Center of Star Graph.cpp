3d95de9f8718af9152024721a266b8d9ad162f33
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2021-08-14 11:00:13 +0530

    Create Find Center of Star Graph.cpp

---
+++ Find Center of Star Graph.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        auto f = edges[0];
+        auto s = edges[1];
+        if(f[0]==s[0] || f[0]==s[1]) return f[0];
+        else return f[1];
+    }
+};
